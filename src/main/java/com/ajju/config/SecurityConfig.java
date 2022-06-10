6f9483110be33ff8091ee8c095cce99ff7f503c7
Laxman <laxmanmule13@gmail.com>
2022-06-10 17:33:34 +0530

    third commit

--- src/main/java/com/ajju/config/SecurityConfig.java
+++ src/main/java/com/ajju/config/SecurityConfig.java
@@ -1,37 +1,37 @@
-//package com.ajju.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-//
-//
-//
-//@SuppressWarnings("deprecation")
-//@EnableWebSecurity
-//@Configuration
-//public class SecurityConfig extends WebSecurityConfigurerAdapter{
-//	
-//	 @Override
-//		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//
-//			 auth.inMemoryAuthentication().withUser("ajju").password("ajju@9075").roles("ADMIN");
-//		}
-//		 @Override
-//			protected void configure(HttpSecurity http) throws Exception {
-//			 http.csrf().disable();
-//			 http.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic();
-//			}
-//		 
-//		 @Bean
-//		 public static NoOpPasswordEncoder passwordEncoder()
-//		 {
-//			 return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-//			 
-//		 }
-//	
-//
-//}
+package com.ajju.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+
+
+@SuppressWarnings("deprecation")
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	 @Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+			 auth.inMemoryAuthentication().withUser("ajju").password("ajju@9075").roles("ADMIN");
+		}
+		 @Override
+			protected void configure(HttpSecurity http) throws Exception {
+			 http.csrf().disable();
+			 http.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic();
+			}
+		 
+		 @Bean
+		 public static NoOpPasswordEncoder passwordEncoder()
+		 {
+			 return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+			 
+		 }
+	
+
+}
